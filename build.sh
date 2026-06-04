67a90235e181d9236d6e2adc029ad05282902609
jiogho <jioghoj@gmail.com>
2026-06-04 21:47:24 +0100

    fix: create db.sqlite3 before migrate

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e  # arrête le script si une commande échoue
+set -e
 
 echo "📦 Installation des dépendances..."
 pip install -r requirements.txt
@@ -7,16 +7,16 @@ pip install -r requirements.txt
 # echo "⚙️ Copie du fichier .env..."
 # if [ ! -f .env ]; then
 #     cp .env.example .env
-#     echo ".env créé depuis .env.example"
-# else
-#     echo ".env existe déjà, pas de remplacement"
 # fi
 
-echo "📁 Collecte des fichiers statiques..."
-python manage.py collectstatic --no-input
+echo "🗄️ Création de la base de données SQLite..."
+touch db.sqlite3
 
 echo "🗄️ Application des migrations..."
 python manage.py migrate
+
+echo "📁 Collecte des fichiers statiques..."
+python manage.py collectstatic --no-input
 
 echo "👤 Création du superuser..."
 python create_superuser.py
