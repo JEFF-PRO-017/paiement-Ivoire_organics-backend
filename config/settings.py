add0c9255b8f772ddae1eeb2d10ef48ca168130f
jiogho <jioghoj@gmail.com>
2026-06-04 22:08:28 +0100

    fix: whitenoise static files + gunicorn timeout

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ← ajoute ici
+
 ]
 
 ROOT_URLCONF = 'config.urls'
