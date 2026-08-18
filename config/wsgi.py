0ce915567cf5be1b6cdee42f7e62cf19c0e4f90e
jiogho <jioghoj@gmail.com>
2026-08-18 13:33:17 +0100

    correction4

--- config/wsgi.py
+++ config/wsgi.py
@@ -3,5 +3,3 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 application = get_wsgi_application()
-
-#TODO FDOSSIER API A SUPPIMER LORS DE LA PRO HORS VERCEL
